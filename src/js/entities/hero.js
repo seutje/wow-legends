437138d5acb7838b5c3a084071c417537cad4a8c
Steve De Jonghe <seutje@gmail.com>
2025-09-08 11:59:01 +0200

    feat: add hero panes to play UI (#8)

--- src/js/entities/hero.js
+++ src/js/entities/hero.js
@@ -1,11 +1,18 @@
 import { shortId } from '../utils/id.js';
 
 export default class Hero {
-  constructor({ id, name = 'Hero', attack = 0, health = 30, armor = 0 } = {}) {
+  constructor({ id, name = 'Hero', data = {}, attack = 0, health = 30, armor = 0, keywords = [], text = '', effects = [] } = {}) {
     this.id = id || shortId('hero');
     this.name = name;
+    if (data) {
+      attack = data.attack ?? attack;
+      health = data.health ?? health;
+      armor = data.armor ?? armor;
+    }
     this.data = { attack, health, armor };
-    this.keywords = [];
+    this.keywords = keywords;
+    this.effects = effects;
+    this.text = text;
     this.equipment = [];
   }
 
