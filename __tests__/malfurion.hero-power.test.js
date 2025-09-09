7ff994d0386ee792b749497857f73d52b4773a86
Steve De Jonghe <seutje@gmail.com>
2025-09-09 20:26:51 +0200

    test: ensure malfurion power presents options (#48)

--- __tests__/malfurion.hero-power.test.js
+++ __tests__/malfurion.hero-power.test.js
@@ -1,9 +1,22 @@
 import fs from 'fs';
+import { jest } from '@jest/globals';
 import Game from '../src/js/game.js';
 import Hero from '../src/js/entities/hero.js';
 
 const cards = JSON.parse(fs.readFileSync(new URL('../data/cards.json', import.meta.url)));
 const malfData = cards.find(c => c.id === 'hero-malfurion-stormrage-archdruid');
+
+test("Malfurion's hero power offers a choice", async () => {
+  const g = new Game();
+  await g.setupMatch();
+  g.player.hero = new Hero(malfData);
+  g.turns.turn = 2;
+  g.resources.startTurn(g.player);
+  const spy = jest.fn(async () => 0);
+  g.promptOption = spy;
+  await g.useHeroPower(g.player);
+  expect(spy).toHaveBeenCalledWith(["+1 ATK this turn", "Gain 2 Armor"]);
+});
 
 test("Malfurion's hero power can grant attack", async () => {
   const g = new Game();
